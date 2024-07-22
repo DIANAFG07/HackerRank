6b7d34d8b9d72aa0762c5eb2a2bf36d6783ced9e
Diana <diana.fg05@hotmail.com>
2024-07-21 20:50:18 -0600

    Test Angular.

---
+++ Angular/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { RecordTableComponent } from './record-table/record-table.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RecordTableComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  exports: [
+    RecordTableComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas : [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule { }
